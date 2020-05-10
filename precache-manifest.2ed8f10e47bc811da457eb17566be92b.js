ff3a80914465006ea1bd494c4db9e2c32e09ea21
George <georgecharleschang@gmail.com>
2020-05-09 22:01:41 -0700

    Updates

---
+++ precache-manifest.2ed8f10e47bc811da457eb17566be92b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758293efbf02a0303b9baa381bee98b1",
+    "url": "/candid.io/index.html"
+  },
+  {
+    "revision": "e59cebc7d604d11101e2",
+    "url": "/candid.io/static/css/main.c79b0d05.chunk.css"
+  },
+  {
+    "revision": "ce23645ff125411d79ce",
+    "url": "/candid.io/static/js/2.d1a38bd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/candid.io/static/js/2.d1a38bd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e59cebc7d604d11101e2",
+    "url": "/candid.io/static/js/main.c0feabab.chunk.js"
+  },
+  {
+    "revision": "eb766deb96b9a1662b85",
+    "url": "/candid.io/static/js/runtime-main.8a005c87.js"
+  },
+  {
+    "revision": "56ebd930d829dff7b5a700c2e7f7727e",
+    "url": "/candid.io/static/media/down-arrow.56ebd930.svg"
+  },
+  {
+    "revision": "7461d5b83ed131a03ef38858b5a21c3c",
+    "url": "/candid.io/static/media/up-arrow.7461d5b8.svg"
+  }
+]);
